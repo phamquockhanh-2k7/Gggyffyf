cfb5c144b959cea78003a77780faa913fe8f81d8
phamquockhanh-2k7 <phamquockhanh2k7@gmail.com>
2025-04-05 16:32:21 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -22,7 +22,7 @@ async def start(update: Update, context: CallbackContext):
         return
     await update.message.reply_text(
         "**👋 Chào mừng bạn.      !**\n"
-        "**🔗 Gửi link bất kỳ để rút gọn.**\n"
+        "**🔗 Gửi link bất kỳ  để rút gọn.**\n"
         "**📷 Chuyển tiếp bài viết kèm ảnh/video, bot sẽ giữ nguyên caption & rút gọn link trong caption.**\n"
         "**💬 Mọi thắc mắc, hãy liên hệ admin.**",
         parse_mode="Markdown"
