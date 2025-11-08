89685843f84777106c2eecbcf13717feeab1e3bb
Jnnknn <phamquockhanh2k7@gmail.com>
2025-11-08 14:45:50 +0700

    Reorder feature registration in bot.py

--- bot.py
+++ bot.py
@@ -12,8 +12,8 @@ def run_bot():
     app = Application.builder().token(BOT_TOKEN).build()
 
     # Đăng ký feature chính
-    register_feature2(app)
     register_feature1(app)
+    register_feature2(app)
     
 
     print("🤖 Bot is running...")
