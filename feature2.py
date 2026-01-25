c73ba41fb0844f3947d34b62050c832065be4716
Jnnknn <phamquockhanh2k7@gmail.com>
2026-01-25 11:43:03 +0700

    Update feature2.py

--- feature2.py
+++ feature2.py
@@ -50,8 +50,8 @@ async def generate_shortened_content(url):
         f"➖➖➖➖➖➖➖➖➖➖\n"
         f"**😘Nếu mua link hãy chọn linkx hoặc anonlink để mua giá rẻ hơn, nếu vượt link hãy dùng oklink, có thể mua nhưng sẽ đắt hơn! **\n\n"
         f"**Cách vượt Link: ** HuongDanVuotLink.vercel.app\n\n"
-        f"**Cách Mua link: ** HuongDanMuaLink.vercel.app \n\n**⫸Lưu lại link này để tránh lạc mất nhau: ``**LinkDuPhongSOS.vercel.app 🥰``"
-        f"**Copy link: ** `LinkDuPhongSOS.vercel.app` "
+        f"**Cách Mua link: ** HuongDanMuaLink.vercel.app \n\n**⫸Lưu lại link này để tránh lạc mất nhau: **LinkDuPhongSOS.vercel.app 🥰\n\n"
+        f"**👉Copy link: ** `LinkDuPhongSOS.vercel.app` "
     )
     return raw_content
 
